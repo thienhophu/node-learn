74a789b1bc3358b1e69716d891b6519e93356f74
Thien Ho <thien.hophu@gmail.com>
2020-09-22 19:58:09 +0700

    u

---
+++ index.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+  const parseUrl = url.parse(req.url, true);
+
+  const path = parseUrl.pathname;
+
+  // Trim '/users/profile' -> 'users/profile',
+  // '/users/' -> 'users,
+  // '/users/profile/' -> 'users/profile'
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  const queryStringObject = parseUrl.query;
+
+  const method = req.method.toLowerCase();
+
+  res.end();
+
+  console.log(
+    `${method.toUpperCase()} - ${path} : ${JSON.stringify(queryStringObject)}`,
+  );
+});
+
+server.listen(3000, () => {
+  console.log('The server is listening on port 3000.');
+});
